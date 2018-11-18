c98a91a2a6f1f24df440040bcef04d21c6b955d8
Trausti Thor Johannsson <traustitj@mac.com>
2018-11-18 20:00:52 +0100

    Adding the final function and some helper functions.
    Also did the capture operator

--- lib/socialid.ex
+++ lib/socialid.ex
@@ -16,6 +16,7 @@ defmodule Socialid do
     :world
   end
 
+  @spec months(integer()) :: [28 | 29 | 30 | 31, ...]
   def months(year) do
     if is_leap_year(year) do
       [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -24,14 +25,16 @@ defmodule Socialid do
     end
   end
 
+  @spec calc_two_arrays([number()], [number()], any()) :: [any()]
   def calc_two_arrays([], [], totals) do
     totals |> Enum.reverse
   end
 
   def calc_two_arrays([head1 | tail1], [head2 | tail2], totals) do
-    calc_two_arrays(tail1, tail2, [head1 + head2 | totals])
+    calc_two_arrays(tail1, tail2, [head1 * head2 | totals])
   end
 
+  @spec is_leap_year(integer()) :: boolean()
   def is_leap_year(year) do
     if rem(year, 400) == 0 or ((rem(year, 4) == 0) and (rem(year, 100) != 0)) do
       true
@@ -40,13 +43,15 @@ defmodule Socialid do
     end
   end
 
-  def get_days_in_month(year, month) do
+  @spec get_days_in_month(integer(), integer()) :: [any()]
+  def get_days_in_month(month, year) do
     days = Enum.at(months(year), month - 1)
     Enum.to_list(1..days)
     |> Enum.map(fn(x) -> Integer.to_string(x) end)
     |> Enum.map(fn(x) -> String.pad_leading(x, 2, "0") end )
   end
 
+  @spec get_full_dates(integer(), any(), any()) :: [any()]
   def get_full_dates(year, month, days) do
     newyear = rem(year, 100) |> Integer.to_string |> String.pad_leading(2, "0")
     newmonth = month |> Integer.to_string |> String.pad_leading(2, "0")
@@ -56,4 +61,65 @@ defmodule Socialid do
       |> Enum.map(fn(x) -> "#{x}#{newmonth}#{newyear}" end)
   end
 
+  def add_counter_to_dates(dates) do
+    endings = 20..99
+    |> Enum.map( fn(x) -> Integer.to_string(x) end )
+    |> Enum.map( fn(x) -> x end)
+
+    dates
+    |> Enum.map(fn(x) -> Enum.map(endings, fn(y) -> "#{x}-#{y}" end) end)
+    |> List.flatten
+  end
+
+  @spec get_checksum(binary()) :: integer()
+  def get_checksum(idnumber) do
+    tmp1 = String.replace(idnumber, "-", "")
+    idnumbers = String.split(tmp1, "", trim: true) |> Enum.map(fn(x) -> String.to_integer(x) end)
+
+    result = calc_two_arrays(idnumbers, [3, 2, 7, 6, 5, 4, 3, 2], []) |> Enum.sum
+    remaining = rem(result, 11)
+    if remaining == 0 do
+      0
+    else
+      11 - remaining
+    end
+  end
+
+  @spec add_checksum_to_id(binary()) :: binary()
+  def add_checksum_to_id(idnumber) do
+    "#{idnumber}#{get_checksum(idnumber)}"
+  end
+
+  @spec get_century_from_year(integer()) :: integer()
+  def get_century_from_year(year) do
+    rem(div(year, 100), 10)
+  end
+
+  @spec add_century_to_id(any(), integer()) :: binary()
+  def add_century_to_id(idnumber, year) do
+    "#{idnumber}#{get_century_from_year(year)}"
+  end
+
+  def is_checksum_legal(idnumber) do
+    x = String.split(idnumber, "-")
+    if String.length(Enum.at(x, 1)) == 3 do
+      true
+    else
+      false
+    end
+  end
+
+  def generate_dates_for_full_month(month, year) do
+    days = 1..Enum.at(months(year), month - 1)
+    get_full_dates(year, month, days)
+  end
+
+  def generate_id_numbers_for_year(year) do
+    Enum.to_list 1..12
+    |> Enum.map(&(generate_dates_for_full_month(&1, year))) |> List.flatten
+    |> add_counter_to_dates
+    |> Enum.map(&add_checksum_to_id(&1))
+    |> Enum.filter(&is_checksum_legal(&1) == true)
+    |> Enum.map(&add_century_to_id(&1, year))
+  end
 end
--- test/socialid_test.exs
+++ test/socialid_test.exs
@@ -7,8 +7,8 @@ defmodule SocialidTest do
   end
 
   test "calculate two arrays" do
-    assert Socialid.calc_two_arrays([1,2,3], [3,2,1], []) == [4,4,4]
-    assert Socialid.calc_two_arrays([1,2,3], [4,2,1], []) == [5,4,4]
+    assert Socialid.calc_two_arrays([1,2,3], [3,2,1], []) == [3,4,3]
+    assert Socialid.calc_two_arrays([1,2,3], [4,2,1], []) == [4,4,3]
   end
 
   test "is leap year" do
@@ -27,11 +27,48 @@ defmodule SocialidTest do
   end
 
   test "get days of month" do
-    assert Socialid.get_days_in_month(2000, 0) == ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
+    assert Socialid.get_days_in_month(0, 2000) == ["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31"]
   end
 
   test "assert that year and months are added to dates" do
     assert Socialid.get_full_dates(2000, 1, [1,2,3]) == ["010100", "020100", "030100"]
+  end
+
+  test "assert that there are 79 items per day added" do
+    list = Socialid.add_counter_to_dates(["010100", "020100", "030100"])
+    assert Enum.at(list, 0) == "010100-20"
+    assert Enum.at(list, 1) == "010100-21"
+    assert Enum.at(list, 79) == "010100-99"
+  end
+
+  test "the checksum should be 3 for social id 030574-30" do
+    assert Socialid.get_checksum("030574-30") == 3
+    assert Socialid.get_checksum("120160-33") == 8
+  end
+
+  test "the checksum is added to the id number" do
+    assert Socialid.add_checksum_to_id("030574-30") == "030574-303"
+    assert Socialid.add_checksum_to_id("120607-40") == "120607-405"
+    assert Socialid.add_checksum_to_id("240305-28") == "240305-283"
+  end
+
+  test "add the century to id number, 9 for 1900 and 0 for 2000" do
+    assert Socialid.get_century_from_year(1987) == 9
+    assert Socialid.get_century_from_year(1887) == 8
+    assert Socialid.get_century_from_year(2087) == 0
+  end
+
+  test "check that the century number is added behind an id number" do
+    assert Socialid.add_century_to_id("030574-303", 1974) == "030574-3039"
+    assert Socialid.add_century_to_id("120607-405", 2007) == "120607-4050"
+    assert Socialid.add_century_to_id("240305-283", 2005) == "240305-2830"
+  end
+
+  test "generate id numbers for a year and check if you find a specific id" do
+    year1974 = Socialid.generate_id_numbers_for_year(1974)
+    assert Enum.find(year1974, fn(x) -> x == "030574-3039" end) == "030574-3039"
+    # year2005 = Socialid.generate_id_numbers_for_year(2005)
+    # assert Enum.find(year2005, fn(x) -> x == "240305-2830" end) == "240305-2830"
   end
 
 end
